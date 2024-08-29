22f0f7707c3582c1ced0a7ba7b3fa4d557fae544
wood <nathanyelw@gmail.com>
2024-08-29 12:26:09 -0400

    brought in serde serializer and deserializer brought in serde_json for, well, s+d with json brought in UUID for build uuid's out of random bytes brought in rand to generate random bytes wrote a test to very it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+//use serde_json::{Deserializer, Serializer};
+
+use rand::RngCore;
+use uuid::Uuid;
+
+use serde::{Serialize, Deserialize};
+#[derive(Debug, Serialize, Deserialize)]
+struct Post {
+    name: String,
+    creator: String,
+    identifier: String
+}
+
+
+impl Post {
+    fn new(name: String, creator: String, id: Uuid) -> Self {
+        Self {
+            name,
+            creator,
+            identifier: id.to_string()
+        }
+    }
+}
+
+fn generate_uuid() -> Uuid {
+    let mut rng = rand::thread_rng();
+    let mut bytes: [u8; 16] = [0;16];
+    rng.fill_bytes(&mut bytes);
+    let uuid = uuid::Uuid::from_bytes(bytes);
+    uuid
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let first_post = Post::new("arbitrary".to_string(), "random".to_string(), generate_uuid());
+    println!("Hello, {}!", first_post.identifier);
+    println!("name: {}, creator: {}", first_post.name, first_post.creator);
+
+    let json = serde_json::to_string(&first_post).expect("error; couldn't serialize");
+    println!("as json: {}", json);
+    let clone_from_serialized: Post = serde_json::from_str(&json).expect("error; couldn't deserialize");
+    println!("after deserializing: {clone_from_serialized:?}");
 }
