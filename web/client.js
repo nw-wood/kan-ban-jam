965dbe90e7b6600e50618b909eccf01aa2a87cb8
nw-wood <nathanyelw@gmail.com>
2024-10-01 22:46:46 -0400

    js client sorting items into div blocks now

--- web/client.js
+++ web/client.js
@@ -1,34 +1,41 @@
 const socket = new WebSocket('ws://192.168.1.169:3032/ws');
 
 console.log('hello from js!');
-// Listen for messages from the server
-socket.onmessage = function(event) {
-    console.log('Message from server:', event.data);
 
-    //handle responses from the server here!
+socket.onmessage = function(event) {
+
+    console.log('Message from server:', event.data);
 
     let jsonObj = JSON.parse(event.data);
 
-    //update the name for the board on the page
     document.getElementById('board-name').innerHTML = jsonObj.board_name;
 
     for(let current_status = 0; current_status < jsonObj.statuses.length; current_status++) {
+
         let status = jsonObj.statuses[current_status];
+
+        let col_by_id = document.getElementById('status-columns');
+
+        col_by_id.innerHTML += '<div id="status-' + status + '">'+status+'<div id = "item-box-'+status+'"></div>';
+
         for (let current_item = 0; current_item < jsonObj.items.length; current_item++) {
+
             let item = jsonObj.items[current_item];
-            //console.log(item);
-            //if item.status == status
+
+            if (item.status == status) {
+                console.log(item.name + ' had status ' + status);
+                let item_box_by_id = document.getElementById('item-box-'+status);
+                item_box_by_id.innerHTML += item.name + '<br>';
+            }
         }
-        document.getElementById('status-columns').innerHTML = document.getElementById('status-columns').innerHTML + '<div class="status">' + jsonObj.statuses[current_status] + '</div>';
     }
 
-    //little debug output below the stuff
-    document.getElementById('server-output').innerHTML = document.getElementById('server-output').innerHTML + event.data + ' </br>';
+    document.getElementById('server-output').innerHTML += event.data + ' </br>';
 
 };
 
-// When the connection opens, send a message
 socket.onopen = function() {
+
     const onopen_response = {
         value: 'ready'
     }
@@ -36,16 +43,12 @@ socket.onopen = function() {
     let req_as_json = JSON.stringify(onopen_response);
 
     socket.send(req_as_json);
-
-    //socket.send('Hello from the client!');
 };
 
-// Handle connection errors
 socket.onerror = function(error) {
     console.error('WebSocket error:', error);
 };
 
-// Handle connection closure
 socket.onclose = function() {
     console.log('WebSocket connection closed');
 };
